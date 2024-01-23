20e05c6571e9edfd544ec5428b5b52302c5a00a5
Israel Alencar de França <alenc4r25@gmail.com>
2024-01-23 16:26:18 -0400

    feat:adicionado mecanismo de criacao de pedido

--- src/main/java/br/com/microservice/loja/client/InfoFornecedorClient.java
+++ src/main/java/br/com/microservice/loja/client/InfoFornecedorClient.java
@@ -1,15 +1,23 @@
 package br.com.microservice.loja.client;
 
+import java.util.List;
+
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import br.com.microservice.loja.controller.dto.InfoFornecedorDTO;
+import br.com.microservice.loja.controller.dto.InfoPedidoDTO;
+import br.com.microservice.loja.controller.dto.IntemDaCompraDTO;
 
 @FeignClient("fornecedor")
 public interface InfoFornecedorClient {
 
 	@GetMapping("/info/{estado}")
 	InfoFornecedorDTO getRetornaFornecedor(@PathVariable String estado);
+
+	@PostMapping("/pedido")
+	InfoPedidoDTO realizaCompra(List<IntemDaCompraDTO> itens);
 
 }
