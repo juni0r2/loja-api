75c3b45c63ade92a38833bdfbb545cb7f3fb6497
Israel Alencar de França <alenc4r25@gmail.com>
2024-01-24 09:40:52 -0400

    feat:adicionado log na aplicacao

--- src/main/java/br/com/microservice/loja/service/CompraService.java
+++ src/main/java/br/com/microservice/loja/service/CompraService.java
@@ -1,5 +1,8 @@
 package br.com.microservice.loja.service;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,8 +12,11 @@ import br.com.microservice.loja.controller.dto.InfoFornecedorDTO;
 import br.com.microservice.loja.controller.dto.InfoPedidoDTO;
 import br.com.microservice.loja.model.Compra;
 
+
 @Service
 public class CompraService {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(CompraService.class);
 
 	@Autowired
 	InfoFornecedorClient client;
@@ -22,9 +28,11 @@ public class CompraService {
 	
 	public Compra realizaCompra(CompraDTO compra) {
 		
+		LOG.info("buscando informações do fornecedor de {}", compra.getEndereco().getEstado() );
 		InfoFornecedorDTO dto = this.client.getRetornaFornecedor(compra.getEndereco().getEstado());
 		System.out.println(dto.getEndereco());
 
+		LOG.info("realizando um pedido");
 		InfoPedidoDTO infoPedidoDTO = this.client.realizaCompra(compra.getItens());
 		
 		Compra compraSalva = new Compra();
